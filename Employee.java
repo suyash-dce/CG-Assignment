a7a082525b63afe78260e9dacc8ea7b2de6e5f26
Suyash Jain <jain.suyash28@gmail.com>
2020-09-27 18:57:58 +0530

    Added: Company Specific Functionality is Added

--- Employee.java
+++ Employee.java
@@ -1,52 +1,93 @@
-public class employee {
+import java.util.Scanner;
+public class Employee {
 	
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
-	public static final int DAILY_HOURLY_WAGE=20;
-	public static final int MIN_DAYS=20;
+	int EmpRatePerHour=20;
+	int MaxDays;
+	int MaxHours;
+	String company;
+	int TotalEmpWage;
 	
+	public Employee(String company,int EmpRatePerHour,int maxDays,int maxHours) {
+		this.company=company;
+		this.EmpRatePerHour=EmpRatePerHour;
+		this.MaxHours=MaxHours;
+		this.MaxDays=MaxDays;
+	}
+	public int empAttendance() {
+		int empCheck=(int) Math.floor(Math.random()*10)%3;
+		return empCheck;
+	}
+	public void welcomeMessage() {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+	public void checkAttendence(){
+		switch (empAttendance()) {
+		case IS_FULL_TIME:
+			System.out.println("Employee is full time");
+			break;
+		case IS_PART_TIME:
+			System.out.println("Employee is part time");
+			break;
+		default:
+			System.out.println("Employee is absent");
+		}
+	}
+	public int daliyWage(int n) {
+		final int partTimeHours=4;
+	        final int fullTimeHours=8;
+		int empWage=0;
+		switch (n) {
+		case IS_FULL_TIME:
+			empWage = (EmpRatePerHour*fullTimeHours);
+			break;
+		case IS_PART_TIME:
+			empWage= (EmpRatePerHour*partTimeHours);
+			break;
+		default:
+			break;
+		}
+		return empWage;
+	}
+	public void monthlyWage() {
+		final int partTimeHours=4;
+                final int fullTimeHours=8;
+		int days = 0;
+		int hours=0;
+		int monthlyWage = 0;
+		while (hours<=MaxHours || days<=MaxDays) {
+			int n=empAttendance();
+			switch (n) {
+			case IS_FULL_TIME:
+				monthlyWage+=daliyWage(n);
+				hours+=fullTimeHours;
+				break;
+			case IS_PART_TIME:
+				monthlyWage+=daliyWage(n);
+				hours+=partTimeHours;
+				break;
+			default:
+				break;
+			}
+			days++;
+		}
+		this.TotalEmpWage=monthlyWage;
+	}
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to the Employee Wage Computation Program.");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Company Name: ");
+		String name= sc.nextLine();
+		System.out.println("Enter Rate Per Hour: ");
+		int RatePerHour= sc.nextInt();
+		System.out.println("Enter Days Worked: ");
+		int DaysWorked= sc.nextInt();
+		System.out.println("Enter Hours Worked: ");
+		int HoursWorked= sc.nextInt();
+		Employee EmployeeWageFn = new Employee(name,RatePerHour,DaysWorked,HoursWorked);
+		EmployeeWageFn.monthlyWage();
+		System.out.println("Dailys Wage of "+name+"'s Employee  is "+EmployeeWageFn.TotalEmpWage); 
 		
-		int EmpWage=0;
-		int EmpHours=0;
-		int EmpIncome =0;
-		int EmpSalary=0;
-		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		int DaysWorked=(int)Math.floor(Math.random()*100)%21;
-		
-		switch(empCheck) {
-			case IS_PART_TIME:
-				System.out.println("The part-time employee is present.");
-				EmpHours=8;
-				EmpIncome=DAILY_HOURLY_WAGE*EmpHours*DaysWorked;
-				System.out.println("The wage till the condition is reached is "+EmpIncome+".");
-				break;
-			case IS_FULL_TIME:
-				System.out.println("The full-time employee is present.");
-			    EmpHours=8;
-			    int HOURS_LIMIT=100;
-				EmpIncome=DAILY_HOURLY_WAGE*HOURS_LIMIT;
-			    System.out.println("The wage till the condition is reached is "+EmpIncome+".");
-			    break;
-			default:
-				System.out.println("The employee is absent.");
-		}
-		
-		EmpWage=calculateWage(EmpHours,DAILY_HOURLY_WAGE);
-		System.out.println("The daily employee wage is "+EmpWage+".");
-		
-		EmpSalary=calculateSalary(EmpHours,DAILY_HOURLY_WAGE,MIN_DAYS);
-		System.out.println("The monthly employee wage is "+EmpSalary+".");
-	
 	}
-	
-	public static int calculateWage(int hours,int hourlyWage) {
-		return hours*hourlyWage;
-	}
-	
-	public static int calculateSalary(int hours,int hourlyWage,int days) {
-		return hours*hourlyWage*days;
-	}
-
+}
