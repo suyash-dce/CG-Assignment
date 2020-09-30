73034f5d156b526d4d8bbb924586d6f2eb717c1b
Suyash Jain <jain.suyash28@gmail.com>
2020-09-30 11:49:23 +0530

    Merging: Daily Wage to Total Wage Function is Merged to EmpWage Program

--- CompanyEmpWage.java
+++ CompanyEmpWage.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.util.*;
 public class CompanyEmpWage {
 	
 	public final String companyName;
@@ -5,17 +7,25 @@ public class CompanyEmpWage {
 	public final int maxWorkingDays;
 	public final int maxWorkingHours;
 	public int totalEmpWage;
-	
+	public ArrayList<Integer> dailyWageList;	
 	public CompanyEmpWage(String companyName, int wagePerHour, int maxWorkingDays, int maxWorkingHours) {
 		
 		this.companyName = companyName;
 		this.wagePerHour = wagePerHour;
 		this.maxWorkingDays = maxWorkingDays;
 		this.maxWorkingHours = maxWorkingHours;
+		this.dailyWageList=new ArrayList<Integer>();
 	}
 	
 	public void setTotalEmpWage(int totalEmpWage) {
 		this.totalEmpWage = totalEmpWage;
+	}
+	public void getDailyWage() {			//For UC 13 and 14
+		System.out.println(dailyWageList); 
+	}
+	
+	public void setDailyWage(int DailyWage) {			//For UC 13 and 14
+		dailyWageList.add(DailyWage);
 	}
 	
 	public String toString() {
